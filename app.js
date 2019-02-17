9cd9d59df9f300c6dffa40c9316f679162eb8c60
Brian Baccarella <baccarellab@gmail.com>
2019-02-17 10:49:44 -0500

    add post put delete

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 const http = new EasyHTTP;
 
-//Get users instead of posts
-http.get('https://json.placehodlers.typicode.com/users');
+// Get Users
+http.get('https://jsonplaceholder.typicode.com/users')
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
+
+const data = {
+  name: 'bb',
+  username: 'bbb',
+  email: 'tt'
+}
+
+// Create Post
+http.post('https://jsonplaceholder.typicode.com/users', data)
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
+// Update Post
+http.put('https://jsonplaceholder.typicode.com/users/2', data)
+.then(data => console.log(data))
+.catch(err => console.log(err));
+
+http.delete('https://jsonplaceholder.typicode.com/users/2')
+.then(data => console.log(data))
+.catch(err => console.log(err));
--- easyhttp2.js
+++ easyhttp2.js
@@ -15,4 +15,52 @@ class EasyHTTP {
     .then(data => console.log(data))
     .catch(err => console.log(err));
   }
+
+
+  // Make an HTTP POST Request
+  post(url, data){
+    return new Promise((resolve, reject) => {
+     fetch(url, {
+       method: 'POST',
+       headers: {
+         'Content-type': 'application/json'
+       },
+       body: JSON.stringify(data)
+     })
+       .then(res => res.json())
+       .then(data => resolve(data))
+       .catch(err => reject(err));
+    });
+  }
+
+   // Make an HTTP PUT Request
+   put(url, data){
+    return new Promise((resolve, reject) => {
+     fetch(url, {
+       method: 'PUT',
+       headers: {
+         'Content-type': 'application/json'
+       },
+       body: JSON.stringify(data)
+     })
+       .then(res => res.json())
+       .then(data => resolve(data))
+       .catch(err => reject(err));
+    });
+  }
+
+  // Make an HTTP DELETE Request
+  delete(url){
+    return new Promise((resolve, reject) => {
+     fetch(url, {
+       method: 'DELETE',
+       headers: {
+         'Content-type': 'application/json'
+       }
+     })
+       .then(res => res.json())
+       .then(data => resolve('Resource Deleted'))
+       .catch(err => reject(err));
+    });
+  }
 }
