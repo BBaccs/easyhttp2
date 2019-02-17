933117cf1fcc8888b522ebbd557b3d810e125060
Brian Baccarella <baccarellab@gmail.com>
2019-02-17 11:42:32 -0500

    update

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const http = new EasyHTTP;
 
 // Get Users
-http.get('https://jsonplaceholder.typicode.com/users')
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// });
 
 const data = {
   name: 'bb',
@@ -12,15 +16,28 @@ const data = {
 }
 
 // Create Post
-http.post('https://jsonplaceholder.typicode.com/users', data)
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(post);
+  }
+});
 
 // Update Post
-http.put('https://jsonplaceholder.typicode.com/users/2', data)
-.then(data => console.log(data))
-.catch(err => console.log(err));
+// http.put('https://jsonplaceholder.typicode.com/posts/5', data, function(err, post) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
 
-http.delete('https://jsonplaceholder.typicode.com/users/2')
-.then(data => console.log(data))
-.catch(err => console.log(err));
+// Delete Post
+// http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(response);
+//   }
+// });
